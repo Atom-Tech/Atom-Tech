46aeded915edad70a636cab8c29bf27c585a5860
thiviscar <thiviscar@gmail.com>
2016-11-08 18:35:16 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<title>Página PHP</title>
+	</head>
+
+	<body bgcolor = "#00CCFF">
+		<?php
+				print "<center>";
+				echo "Minha página em PHP";
+				print "<br>";
+        		print "Minha página em PHP";
+				print "</center>";
+		?>
+    </body>
+
+</html>
